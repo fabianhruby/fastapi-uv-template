ee1778301962b81c797347ee8eb16cae4d97d92f
Fabian Hruby <fabian.hruby@gmail.com>
2026-02-24 20:16:30 +0100

    Add a test for the `/live` endpoint

--- tests/core/routers/test_health.py
+++ tests/core/routers/test_health.py
@@ -13,3 +13,10 @@ def test_health_check():
     response = client.get("/health")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"status": "ready"}
+
+
+def test_live_check():
+    """Test the live check endpoint."""
+    response = client.get("/live")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"status": "alive"}
